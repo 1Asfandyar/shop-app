02ad562d8d02a55a0e3a6b2adfd83b060e24993b
Abdur Rahman <abdur@lycus.com>
2023-07-04 00:24:15 +0500

    Category and Prodcut Assosiactions is created.

---
+++ db/migrate/20230703185256_create_category_and_product_associations.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# CreateCategoryAndProductAssociations
+class CreateCategoryAndProductAssociations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :categories_products, id: false do |t|
+      t.belongs_to :product
+      t.belongs_to :category
+      t.timestamps
+    end
+  end
+end
