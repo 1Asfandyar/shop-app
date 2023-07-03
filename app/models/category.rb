09a9f69f1965a4f315b06f4d62e403defdb5407f
Abdur Rahman <abdur@lycus.com>
2023-07-04 00:22:52 +0500

    Category model is created.

---
+++ app/models/category.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Category model
+class Category < ApplicationRecord
+  has_many :subcategories, class_name: 'Category', foreign_key: 'parent_id'
+  belongs_to :parent, class_name: 'Category', optional: true
+
+  has_and_belongs_to_many :products
+
+  validates :name, presence: true
+end
