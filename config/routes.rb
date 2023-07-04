d38390be2907de89048175043bedfc020853d19c
Abdur Rahman <abdur@lycus.com>
2023-07-04 20:46:40 +0500

    routes resources categories is decleared.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'home#index'
 
-  resource :categories
+  resources :categories, except: :index
 end
