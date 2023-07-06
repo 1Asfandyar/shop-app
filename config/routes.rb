ba307970f642a2eed934b7e4b16b1e9a53367e9f
1Asfandyar <masfandyar44@gmail.com>
2023-07-06 19:29:28 +0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # root 'home#index'
+  # root 'products#index'
+
   root 'products#show'
 
   resources :categories, except: :index
