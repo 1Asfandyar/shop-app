f9cba85eeb1d8369f6a6ce39401ecbd48850790d
1Asfandyar <masfandyar44@gmail.com>
2023-07-14 02:48:58 +0500

    code for add to cart

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,15 @@
+class CartController < ApplicationController
+    def create_cookie
+        @product = Product.find_by(cart_params) 
+        cookies[:cart_id] = @product.code
+    end
+
+    def index
+        
+    end
+
+private
+    def cart_params
+        params.permit(:id)
+    end
+end
