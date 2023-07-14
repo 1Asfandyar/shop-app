4c2c674d5594d8b2d5deb11b992c90192af8c67d
Abdur Rahman <abdur@lycus.com>
2023-07-14 21:21:57 +0500

    mailer updated to accept inqury object id.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,7 +2,8 @@
 
 # OrderMailer
 class OrderMailer < ApplicationMailer
-  def new_order
+  def new_order(inquiry_obj_id)
+    @inquiry_obj = ProductInquiry.find_by(id: inquiry_obj_id)
     mail(subject: 'New Order')
   end
 end
