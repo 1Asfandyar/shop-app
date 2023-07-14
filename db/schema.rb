13d8d9a50b37a681952ef2786bb2767b9298923d
Abdur Rahman <abdur@lycus.com>
2023-07-14 22:56:45 +0500

    db is migrated.

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_150935) do
+ActiveRecord::Schema[7.0].define(version: 20_230_714_165_950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -64,23 +64,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_150935) do
     t.index ['product_id'], name: 'index_categories_products_on_product_id'
   end
 
-  create_table "product_inquiries", force: :cascade do |t|
-    t.bigint "product_id", null: false
-    t.string "email"
-    t.text "comments"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_product_inquiries_on_product_id"
+  create_table 'product_inquiries', force: :cascade do |t|
+    t.bigint 'product_id', null: false
+    t.string 'email'
+    t.text 'comments'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['product_id'], name: 'index_product_inquiries_on_product_id'
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "code", default: "", null: false
-    t.text "description", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["code"], name: "index_products_on_code", unique: true
-    t.index ["name"], name: "index_products_on_name", unique: true
   create_table 'products', force: :cascade do |t|
     t.string 'name', default: '', null: false
     t.string 'code', default: '', null: false
@@ -89,6 +81,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_150935) do
     t.datetime 'updated_at', null: false
     t.index ['code'], name: 'index_products_on_code', unique: true
     t.index ['name'], name: 'index_products_on_name', unique: true
+  end
+
+  create_table 'roles', force: :cascade do |t|
+    t.string 'name', default: 'other', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['name'], name: 'index_roles_on_name', unique: true
+  end
+
+  create_table 'user_roles', force: :cascade do |t|
+    t.bigint 'role_id', null: false
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['role_id'], name: 'index_user_roles_on_role_id'
+    t.index %w[user_id role_id], name: 'index_user_roles_on_user_id_and_role_id', unique: true
+    t.index ['user_id'], name: 'index_user_roles_on_user_id'
   end
 
   create_table 'users', force: :cascade do |t|
@@ -105,7 +114,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_150935) do
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "product_inquiries", "products"
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'product_inquiries', 'products'
+  add_foreign_key 'user_roles', 'roles'
+  add_foreign_key 'user_roles', 'users'
 end
