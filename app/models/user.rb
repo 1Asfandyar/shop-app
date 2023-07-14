79723d3c8112739292f63b810ec5ccb5b2c9e984
Abdur Rahman <abdur@lycus.com>
2023-07-14 22:55:52 +0500

    user role is added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :user_role
+  delegate :role, to: :user_role
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates_format_of :email, with: /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i
