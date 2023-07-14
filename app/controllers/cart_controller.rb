513f09e0dd0c9f491e56537dc0d1fda75a68eddb
Abdur Rahman <abdur@lycus.com>
2023-07-14 21:28:27 +0500

    converted 4 spaces to 2 space.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
+# CartController
 class CartController < ApplicationController
-    def create_cookie
-        @product = Product.find_by(cart_params) 
-        cookies[:cart_id] = @product.code
-    end
+  def create_cookie
+    @product = Product.find_by(cart_params)
+    cookies[:cart_id] = @product.code
+  end
 
-    def index
-        
-    end
+  def index; end
 
-private
-    def cart_params
-        params.permit(:id)
-    end
+  private
+
+  def cart_params
+    params.permit(:id)
+  end
 end
