cf7b605199a4846b782ae82e1382a9e8a9c381be
1Asfandyar <masfandyar44@gmail.com>
2023-07-15 17:28:09 +0500

    added cart actions

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,14 +1,35 @@
 class CartController < ApplicationController
+    require 'cgi'
+    before_action :find_product, only: [:create_cookie, :remove_item]
+    
     def create_cookie
+        existing_values = ["#{cookies[:cart_id]}"]
         @product = Product.find_by(cart_params) 
-        cookies[:cart_id] = @product.code
+        unless CGI.unescape(existing_values[0]).include?("#{@product.code}")
+            cookie_value = existing_values + ["#{@product.code}"]
+            cookies[:cart_id] = cookie_value
+        end
     end
 
-    def index
-        
+    def cart_items
+        return @cart_product = nil if cookies[:cart_id].nil?
+        @cart_products = Product.where(code: cookies[:cart_id].split("&"))
     end
 
-private
+    def remove_item
+        existing_values = ["#{cookies[:cart_id]}"]
+        if CGI.unescape(existing_values[0]).include?("#{@product.code}")
+            cookie_value = existing_values[0].split("&").reject { |item| item == "#{@product.code}" }
+            cookies[:cart_id] = cookie_value
+        end
+    end
+
+    private
+
+    def find_product
+        @product = Product.find_by(cart_params)
+    end
+
     def cart_params
         params.permit(:id)
     end
