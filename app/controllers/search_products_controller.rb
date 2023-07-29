4f3e70bfb7700039fd581c62833a7a2f20086bc0
1Asfandyar <masfandyar44@gmail.com>
2023-07-29 17:52:29 +0500

    added pagination and seacrh product

--- app/controllers/search_products_controller.rb
+++ app/controllers/search_products_controller.rb
@@ -1,2 +1,15 @@
 class SearchProductsController < ApplicationController
+    def index
+        @categories = Category.with_attached_cover_image.only_root
+        @products = if params[:search]
+            Product.where('name ILIKE ?', "%#{params[:search]}%").paginate(page: params[:page], per_page: 10)
+          else
+            Product.paginate(page: params[:page], per_page: 10)
+          end
+    end
+    private
+
+    def product_params
+      params.require(:search).permit(:search)
+    end
 end
