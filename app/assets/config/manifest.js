eca4b3c376b6da4b66b7ada686d17388f7f3efab
1Asfandyar <masfandyar44@gmail.com>
2023-08-02 00:01:10 +0500

    added flash messages

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,8 @@
 //= link_tree ../builds
 //= link application.scss
 //= link application.bootstrap.scss
-//= require rails-ujs
+
+//= link flash_messages
 //= link scroll_to_new_arrival.js
 //= link remove_cart_item
 
